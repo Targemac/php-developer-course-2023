e75886e368911e31b30f17ccbc32c37dd1f17436
targemac <targemac@gmail.com>
2023-09-28 16:39:32 +0100

    dockblocks

--- OOP/App/Utility.php
+++ OOP/App/Utility.php
@@ -4,6 +4,17 @@ namespace App;
 
 class Utility
 {
+
+    // docblock (used to document our code)
+    /**
+     * Neatly prints an array
+     * 
+     * OUtputs array formatted by pre tags for formatting
+     * 
+     * @param array $array The array to output
+     * 
+     */
+
     // creating a static method
     public static function printArr(array $array)
     {
